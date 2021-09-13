325c206c7c8f2b1fcb2243e0fde7c534a0075c8b
mustafa <hamandoshm@hotmail.com>
2021-09-13 15:20:57 -0300

    feat: create global theme

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Dashboard} from "./src/screens/Dashboard";
+import {ThemeProvider} from "styled-components";
+import theme from './src/global/styles/theme'
 
 export default function App() {
     return (
         <>
-            <Dashboard/>
+            <ThemeProvider theme={theme}>
+                <Dashboard/>
+            </ThemeProvider>
         </>
     );
 }
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,15 @@
+export default {
+    colors: {
+        primary_color: '#5636D3',
+        secondary_color: '#FF872C',
+        secondary_color_light: 'rgba(255, 135, 44, 0.3)',
+        success: '#12A454',
+        success_light: 'rgba(18, 164, 84, 0.5)',
+        alert: '#E83F5B',
+        alert_light: 'rgba(232, 63, 91, 0.5)',
+        shape: '#FFF',
+        title: '#363F5F',
+        text: '#969CB2',
+        background: '#F0F2F5',
+    }
+}
