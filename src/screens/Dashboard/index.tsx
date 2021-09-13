bb1c5ea9f8fe48f0105c1de3e5216f983e35a256
mustafa <hamandoshm@hotmail.com>
2021-09-13 20:43:12 -0300

    feat: creat header with user info

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import {Container, Title} from "./styles";
+import {Container, Header, Icon, ProfilePicture, User, UserGreeting, UserInfo, UserName, UserWrapper} from "./styles";
 
 export const Dashboard = () => {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <ProfilePicture source={{uri: 'https://github.com/mustafahamandosh.png'}}/>
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Mustafa</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name='power'/>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
